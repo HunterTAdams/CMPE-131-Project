10684a0f6ea4018ef21e8ce50713387c43051133
Shane Nguyen <sn103103@gmail.com>
2022-10-03 22:50:34 -0700

    Wrote a HelloWorld script, attached it to a DeleteMe game object in SampleScene

---
+++ CMPE 131 Project/Assets/HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // I am a script attached to the empty object called "DeleteMe" and I print Hello World to the console during runtime
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
